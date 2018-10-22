448f1e2d43c0be6267e082d6a50e586f577e1205
Sith <raveningly@gmail.com>
2018-10-22 15:52:27 +0300

    I embrace death.
    Added a simple JSP GET endpoint that outputs a message on screen. Attempting to solve server shit, cuz apparently I have the memory of a goldfish.

---
+++ src/main/java/kg/todolist/MainController.java
@@ -0,0 +1,22 @@
+package kg.todolist;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @Value("${home.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "/home";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/home.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+</head>
+
+<body>
+    <h1>Welcome to ${message} </h1> 
+</body>
+
+</html>
